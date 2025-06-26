bae427ab1a8d5a71fb055d9b99534682805c3a27
ndcalcs <carrow8993@gmail.com>
2025-06-25 19:58:54 -0700

    v1.0
    updated a ton of stuff in preparation for v1.0

--- statcalcs.js
+++ statcalcs.js
@@ -1,11 +1,15 @@
 const digimonDict = {
   "Botamon": { "HP": 50, "ATK": 6, "SPD": 4, "DEF": 6 },
+  "Chibickmon": { "HP": 40, "ATK": 7, "SPD": 6, "DEF": 4 },
   "Koromon": { "HP": 90, "ATK": 9, "SPD": 6, "DEF": 6 },
+  "Pickmon": { "HP": 70, "ATK": 10, "SPD": 8, "DEF": 5 },
   "Agumon": { "HP": 250, "ATK": 50, "SPD": 50, "DEF": 25 },
   "Agumon 06": { "HP": 250, "ATK": 55, "SPD": 55, "DEF": 25 },
   "Black Agumon": { "HP": 300, "ATK": 55, "SPD": 40, "DEF": 25 },
+  "Gottsumon": { "HP": 400, "ATK": 35, "SPD": 35, "DEF": 40 },
   "Kokuwamon": { "HP": 450, "ATK": 30, "SPD": 30, "DEF": 45 },
   "Pillomon": { "HP": 500, "ATK": 25, "SPD": 20, "DEF": 55 },
+  "Starmons": { "HP": 300, "ATK": 40, "SPD": 60, "DEF": 20 },
   "Yuki Agumon": { "HP": 350, "ATK": 40, "SPD": 45, "DEF": 30 },
   "Clockmon": { "HP": 1050, "ATK": 80, "SPD": 65, "DEF": 50 },
   "DarkTyrannomon": { "HP": 1050, "ATK": 80, "SPD": 65, "DEF": 50 },
@@ -45,19 +49,44 @@ const digimonDict = {
   "Polar Bearmon": { "HP": 2300, "ATK": 110, "SPD": 100, "DEF": 210 },
   "RizeGreymon": { "HP": 1950, "ATK": 230, "SPD": 140, "DEF": 110 },
   "Triceramon": { "HP": 2100, "ATK": 125, "SPD": 75, "DEF": 190 },
-  "WaruMonzaemon": { "HP": 2150, "ATK": 245, "SPD": 145, "DEF": 145 }
+  "WaruMonzaemon": { "HP": 2150, "ATK": 245, "SPD": 145, "DEF": 145 },
+  "Icemon": {"HP": 1000, "ATK": 60, "SPD": 50, "DEF": 90},
+  "ShootingStarmon": {"HP": 900, "ATK": 120, "SPD": 75, "DEF": 40},
+  "Starmon": {"HP": 1100, "ATK": 70, "SPD": 70, "DEF": 50},
+  "DarkSuperStarmon": {"HP": 2000, "ATK": 150, "SPD": 90, "DEF": 160},
+  "SuperStarmon": {"HP": 1900, "ATK": 160, "SPD": 160, "DEF": 90},
+  "InsekimonHigh": {"HP": 2000, "ATK": 160, "SPD": 140, "DEF": 200},
+  "InsekimonLow": {"HP": 2000, "ATK": 150, "SPD": 90, "DEF": 160},
+  "InsekimonSuper": {"HP": 1900, "ATK": 160, "SPD": 160, "DEF": 140},
+  "InsekimonDark": {"HP": 2000, "ATK": 150, "SPD": 90, "DEF": 210},
+  "Daipenmon": {"HP": 1200, "ATK": 160, "SPD": 110, "DEF": 160},
+  "MetalEtemon": {"HP": 1300, "ATK": 179, "SPD": 76, "DEF": 163},
+  "PlatinumNumemon": {"HP": 2000, "ATK": 100, "SPD": 60, "DEF": 190},
+  "Machinedramon": {"HP": 1700, "ATK": 170, "SPD": 70, "DEF": 140},
+  "Blizzardmon": {"HP": 2100, "ATK": 160, "SPD": 120, "DEF": 160},
+  "Etemon": {"HP": 2000, "ATK": 239, "SPD": 157, "DEF": 152},
+  "Chackmon": {"Type": "Free", "HP": 950, "ATK": 80, "SPD": 70, "DEF": 80}
 };
-const baby1List = ["Botamon"];
-const baby2List = ["Koromon"];
+const baby1List = [
+  "Botamon",
+  "Chibickmon"
+];
+const baby2List = [
+  "Koromon",
+  "Pickmon"
+];
 const childList = [
   "Agumon",
   "Agumon 06",
   "Black Agumon",
+  "Gotsumon",
   "Kokuwamon",
   "Pillomon",
+  "Starmons",
   "Yuki Agumon"
 ];
 const adultList = [
+  "Chackmon",
   "DarkTyrannomon",
   "Deltamon",
   "GeoGreymon",
@@ -72,7 +101,10 @@ const adultList = [
   "Tyrannomon",
   "Vdramon",
   "VDramon Black",
-  "VDramon Red"
+  "VDramon Red",
+  "Icemon",
+  "ShootingStarmon",
+  "Starmon"
 ];
 const perfList = [
   "AeroVdramon",
@@ -94,8 +126,21 @@ const perfList = [
   "Polar Bearmon",
   "RizeGreymon",
   "Triceramon",
-  "WaruMonzaemon"
-];
+  "WaruMonzaemon",
+  "DarkSuperStarmon",
+  "SuperStarmon",
+  "InsekimonHigh",
+  "InsekimonLow",
+  "InsekimonSuper",
+  "InsekimonDark",
+  "Etemon"
+]
+const ultList = [
+  "Daipenmon",
+  "Machinedramon",
+  "MetalEtemon",
+  "PlatinumNumemon"
+]
 const stats = [
     "HP",
     "ATK",
@@ -103,14 +148,19 @@ const stats = [
     "DEF"
 ];
 
+const baby1 = document.getElementById('baby1');
+const baby2 = document.getElementById('baby2');
 const child = document.getElementById('child');
 const adult = document.getElementById('adult');
 const perf = document.getElementById('perf');
+const ult = document.getElementById('ult');
 
-const koroBase = [110,11,8,8];
+const baby1StatList = ['hp1','atk1','spd1','def1'];
+const baby2StatList = ['hp2','atk2','spd2','def2'];
 const childStatList = ['hp3','atk3','spd3','def3'];
 const adultStatList = ['hp4','atk4','spd4','def4'];
 const perfStatList = ['hp5','atk5','spd5','def5'];
+const ultStatList = ['hp6','atk6','spd6','def6'];
 
 function calcBaseStat(currentDigi,lastDigi,stage) {
     var newStats = [];
@@ -119,23 +169,90 @@ function calcBaseStat(currentDigi,lastDigi,stage) {
 
     for (let i = 0; i < stats.length; i++) {
         switch(stage) {
-            case "Child":
-                newStats.push(currentInnate[stats[i]] + koroBase[i] - Math.round(0.6*(lastInnate[stats[i]])));
-                document.getElementById(childStatList[i]).textContent = newStats[i];
-                break;
-            case "Adult":
-                childStat = Number(document.getElementById(childStatList[i]).innerText);
-                newStats.push(currentInnate[stats[i]] + childStat - Math.round(0.6*(lastInnate[stats[i]])));
-                document.getElementById(adultStatList[i]).textContent = newStats[i];
-                break;
-            case "Perfect":
-                adultStat = Number(document.getElementById(adultStatList[i]).innerText);
-                newStats.push(currentInnate[stats[i]] + adultStat - Math.round(0.6*(lastInnate[stats[i]])));
-                document.getElementById(perfStatList[i]).textContent = newStats[i];
-          }
+          case "Baby1":
+            document.getElementById(baby1StatList[i]).textContent = currentInnate[stats[i]];
+            break;
+          case "Baby2":
+            baby1Stat = Number(document.getElementById(baby1StatList[i]).innerText);
+            newStats.push(currentInnate[stats[i]] + baby1Stat - Math.round(0.6*(lastInnate[stats[i]])));
+            document.getElementById(baby2StatList[i]).textContent = newStats[i];
+            break;
+          case "Child":
+            baby2Stat = Number(document.getElementById(baby2StatList[i]).innerText);
+            newStats.push(currentInnate[stats[i]] + baby2Stat - Math.round(0.6*(lastInnate[stats[i]])));
+            document.getElementById(childStatList[i]).textContent = newStats[i];
+            break;
+          case "Adult":
+            childStat = Number(document.getElementById(childStatList[i]).innerText);
+            if (currentDigi == "Scumon") {
+              newStats.push(childStat + Math.round(lastInnate[stats[i]]*0.74));
+            } else {
+              newStats.push(currentInnate[stats[i]] + childStat - Math.round(0.6*(lastInnate[stats[i]])));
+            }
+            document.getElementById(adultStatList[i]).textContent = newStats[i];
+            break;
+          case "Perfect":
+            adultStat = Number(document.getElementById(adultStatList[i]).innerText);
+            if (lastDigi == "Scumon" || currentDigi == "KingScumon") {
+              newStats.push(Math.round(adultStat*1.4));
+            } else {
+              newStats.push(currentInnate[stats[i]] + adultStat - Math.round(0.6*(lastInnate[stats[i]])));
+            }
+            document.getElementById(perfStatList[i]).textContent = newStats[i];
+            break;
+          case "Ultimate":
+            perfStat = Number(document.getElementById(perfStatList[i]).innerText);
+            if (lastDigi == "KingScumon") { 
+              newStats.push(Math.round(1.4*perfStat));
+            } else {
+              newStats.push(currentInnate[stats[i]] + perfStat - Math.round(0.6*(lastInnate[stats[i]])));
+            }
+            document.getElementById(ultStatList[i]).textContent = newStats[i];
+        }
     }
+    updateSums();
 }
 
-child.addEventListener('change', calcBaseStat(child.value,"Koromon","Child"));
+const sumList = ['sum1','sum2','sum3','sum4','sum5','sum6'];
+
+function updateSums() {
+  for (let j = 0; j < sumList.length; j++) {
+    currentSum = 0;
+    for (let i = 0; i < stats.length; i++) {
+      currentStat = 0;
+      switch (j) {
+        case 0:
+          currentStat = Number(document.getElementById(baby1StatList[i]).textContent);
+          break;
+        case 1:
+          currentStat = Number(document.getElementById(baby2StatList[i]).textContent);
+          break;
+        case 2:
+          currentStat = Number(document.getElementById(childStatList[i]).textContent);
+          break;
+        case 3:
+          currentStat = Number(document.getElementById(adultStatList[i]).textContent);
+          break;
+        case 4:
+          currentStat = Number(document.getElementById(perfStatList[i]).textContent);
+          break;
+        case 5:
+          currentStat = Number(document.getElementById(ultStatList[i]).textContent);
+          break;
+      }
+      if (stats[i] == "HP") {
+        currentSum = currentSum + Math.round(currentStat/10);
+      } else {
+        currentSum = currentSum + currentStat;
+      }
+    }
+    document.getElementById(sumList[j]).textContent = currentSum
+  }
+}
+
+baby1.addEventListener('change', calcBaseStat(baby1.value,baby1.value,"Baby1"));
+baby2.addEventListener('change', calcBaseStat(baby2.value,baby1.value,"Baby2"));
+child.addEventListener('change', calcBaseStat(child.value,baby2.value,"Child"));
 adult.addEventListener('change', calcBaseStat(adult.value,child.value,"Adult"));
 perf.addEventListener('change', calcBaseStat(perf.value,adult.value,"Perfect"));
+perf.addEventListener('change', calcBaseStat(ult.value,perf.value,"Ultimate"));
